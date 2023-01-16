7da36bb3086a88f669c7ad4623ad3782817b2a82
Fedor Stolov <122786154+fedorstolov@users.noreply.github.com>
2023-01-16 18:26:23 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('somehow we will make it because that is what we do')
+})
